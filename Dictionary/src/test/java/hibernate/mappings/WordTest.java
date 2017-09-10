e24f4581057db9aa5241bbc254d95bbb21c0056d
Dariusz Lelek <dariusz.lelek@gmail.com>
2017-09-10 13:50:26 +0100

    Added junit for Word class.

---
+++ Dictionary/src/test/java/hibernate/mappings/WordTest.java
@@ -0,0 +1,47 @@
+package hibernate.mappings;
+
+import dictionary.word.WordClass;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WordTest {
+  private Word word;
+
+  @Before
+  public void setUp() throws Exception {
+    word = new Word();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void getWordClass() throws Exception {
+    System.out.println("getWordClass");
+
+    word.setWordClassString(WordClass.NOUN.toString());
+    assertEquals(WordClass.NOUN, word.getWordClass());
+    word.setWordClassString("noun");
+    assertEquals(WordClass.NOUN, word.getWordClass());
+    word.setWordClassString("Noun");
+    assertEquals(WordClass.NOUN, word.getWordClass());
+    word.setWordClassString("some unknown class");
+    assertEquals(WordClass.UNKNOWN, word.getWordClass());
+  }
+
+  @Test
+  public void getSynonymsList() throws Exception {
+    System.out.println("getWordClass");
+
+    String[] synonyms = {"string1", "string2", "string3"};
+    String synonymsString = String.join(",", synonyms);
+
+    word.setSynonyms(synonymsString);
+    assertEquals(synonyms.length, word.getSynonymsList().size());
+  }
+
+}
