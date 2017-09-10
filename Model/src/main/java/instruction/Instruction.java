73aab7b5a76f82f3ec963730bb3938cfc95d2f62
Dariusz Lelek <dariusz.lelek@gmail.com>
2017-09-10 21:38:52 +0100

    Removed numbers from instruction - handle all in words.

--- Model/src/main/java/instruction/Instruction.java
+++ Model/src/main/java/instruction/Instruction.java
@@ -9,31 +9,22 @@ public class Instruction {
   public final static Instruction EMPTY = new Instruction();
 
   private final Queue<Word> words;
-  private final Queue<Number> numbers;
 
   public Instruction(Queue<Word> words, Queue<Number> numbers) {
     this.words = words;
-    this.numbers = numbers;
   }
 
   public Instruction() {
     this.words = null;
-    this.numbers = null;
   }
 
   public Queue<Word> getWords() {
     return words != null ? words : new LinkedList<>();
   }
 
-  public Queue<Number> getNumbers() {
-    return numbers != null ? numbers : new LinkedList<>();
-  }
-
   @Override
   public String toString() {
     return "Instruction{" +
-        "words=" + words +
-        ", numbers=" + numbers +
-        '}';
+        "words=" + words + '}';
   }
 }
