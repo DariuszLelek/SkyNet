1a8af1cf83ef4a4b6c40bff0c2ec423786ade62d
Dariusz Lelek <dariusz.lelek@gmail.com>
2017-09-13 17:57:31 +0100

    Added Email skill.

---
+++ Skills/src/main/java/skills/Email.java
@@ -0,0 +1,97 @@
+/*
+ * Created by Dariusz Lelek on 9/13/17 3:14 PM
+ * Copyright (c) 2017. All rights reserved.
+ */
+
+package skills;
+
+import credentials.Credentials;
+import dictionary.WordClass;
+import process.priority.Priority;
+import skill.Skill;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.*;
+
+public class Email extends Skill {
+
+  private Session mailSession;
+
+  private final static String EMAIL_HOST = "smtp.gmail.com";
+  private final static String EMAIL_TRANSPORT = "smtp";
+  private final static String EMAIL_SUBJECT = "You got email from SkyNet";
+  private final static String EMAIL_FOOTER = "<br><br>Sent by SkyNet in behalf of "
+      + Credentials.ROOT_USER_NAME.getValue() + ".";
+
+  public Email() {
+    super(Priority.LOW, new ArrayList<WordClass>(){{
+      add(WordClass.NOUN);
+      add(WordClass.NOUN);
+    }});
+  }
+
+  @Override
+  public boolean process() {
+    if(hasValidInstruction()){
+      setMailServerProperties();
+
+      // TODO get recipient from DB
+      String recipient = "dariusz.lelek@gmail.com";
+      // TODO change to Content object storing text, audio, picture etc
+      String content = "some content";
+
+      try {
+        sendEmail(getMessage(recipient, content));
+      } catch (javax.mail.MessagingException e) {
+        logFail("Failed to send Email.", e);
+        return false;
+      }
+      logSuccess("Email sent to:" + recipient + ", content:" + content + ".");
+      return true;
+    }
+    return false;
+  }
+
+  private void setMailServerProperties()
+  {
+    Properties emailProperties = System.getProperties();
+
+    emailProperties.put("mail.smtp.host", "smtp.gmail.com");
+    emailProperties.put("mail.smtp.socketFactory.port", "587");
+    emailProperties.put("mail.smtp.socketFactory.class", "javax.net.SocketFactory");
+    emailProperties.put("mail.smtp.port", "586");
+    emailProperties.put("mail.smtp.auth", "true");
+    emailProperties.put("mail.smtp.ssl.enable", "false");
+    emailProperties.put("mail.smtp.starttls.enable", "true");
+    emailProperties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+    mailSession = Session.getDefaultInstance(emailProperties, null);
+  }
+
+  private MimeMessage getMessage(final String recipient, final String content) throws AddressException, javax.mail.MessagingException
+  {
+    MimeMessage emailMessage = new MimeMessage(mailSession);
+    emailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+    emailMessage.setSubject(EMAIL_SUBJECT);
+
+    // TODO refactor to use different body types
+    emailMessage.setContent(content + EMAIL_FOOTER, "text/html");
+    return emailMessage;
+  }
+
+  private void sendEmail(MimeMessage emailMessage) throws AddressException, javax.mail.MessagingException
+  {
+    Transport transport = mailSession.getTransport(EMAIL_TRANSPORT);
+    transport.connect(EMAIL_HOST, Credentials.MAIL_PROCESSOR_USER.getValue(),
+        Credentials.MAIL_PROCESSOR_PASSWORD.getValue());
+
+    transport.sendMessage(emailMessage, emailMessage.getAllRecipients());
+    transport.close();
+  }
+
+}
