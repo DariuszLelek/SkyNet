a8ca32653287317de8f21c503093d0d4cd99229d
Dariusz Lelek <dariusz.lelek@gmail.com>
2017-09-14 23:34:04 +0100

    EmailHelper cleanup.

--- Utility/src/main/java/helper/EmailHelper.java
+++ Utility/src/main/java/helper/EmailHelper.java
@@ -23,20 +23,24 @@ public class EmailHelper {
   private final String[] MAIL_DOT = {".", "dot"};
 
   public String getEmail(Collection<String> chunks) {
-    String candidate = tryGetMailFromChunks(chunks);
+    String candidate;
 
-    // TODO implement strategy pattern
+    candidate = tryGetValidMailFromChunks(chunks);
     if (emailValidator.isValid(candidate)) {
       return candidate;
-    } else {
-      candidate = tryGetValidMailFromPersons(chunks);
-      if (emailValidator.isValid(candidate)) {
-        return candidate;
-      } else {
-        // TODO tryGetValidMailFromChunks()
-        return "";
-      }
     }
+
+    candidate = tryGetValidMailFromPersons(chunks);
+    if (emailValidator.isValid(candidate)) {
+      return candidate;
+    }
+
+    candidate = tryAssembleValidMailFromChunks(chunks);
+    if (emailValidator.isValid(candidate)) {
+      return candidate;
+    }
+
+    return "";
   }
 
   private String tryGetValidMailFromPersons(Collection<String> chunks){
@@ -79,8 +83,12 @@ public class EmailHelper {
     }
   }
 
-  private String tryGetMailFromChunks(Collection<String> chunks){
+  private String tryGetValidMailFromChunks(Collection<String> chunks){
     return chunks.stream().filter(emailValidator::isValid).findFirst().orElse("");
   }
 
+  private String tryAssembleValidMailFromChunks(Collection<String> chunks){
+    // TODO;
+    return "";
+  }
 }
