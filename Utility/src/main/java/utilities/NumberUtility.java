6adc5433e84d79d3d85c3170ac837c5fcc3d1af4
Dariusz Lelek <dariusz.lelek@gmail.com>
2017-09-16 21:51:22 +0100

    NumberUtility cleanup.

--- Utility/src/main/java/utilities/NumberUtility.java
+++ Utility/src/main/java/utilities/NumberUtility.java
@@ -15,65 +15,108 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class NumberUtility {
-  private static final Logger logger = Logger.getLogger(NumberUtility.class);
-
   private final static String[] keyWords =
       {"number", "cipher", "numeration", "number", "digit", "symbol", "sum", "unit"};
   private final static String conjunction = "and";
   private final static long minimumMatchCount = 2;
   private final static WordClass numberClass = WordClass.NOUN;
-
   private final static Map<String, Long> CACHE = new HashMap<>();
 
-  public static float getRatioMinToMax(int first, int second){
+  public static float getRatioMinToMax(int first, int second) {
     int min = Math.min(first, second);
     int max = Math.max(first, second);
-    return (float) min/max;
+    return (float) min / max;
   }
 
-  public static List<Long> tryGetNumbersFromWords(Collection<String> words){
-    List<Long> numbers = words.stream().filter(word -> !word.equals(conjunction))
+  public static List<Long> tryGetNumbersFromWords(Collection<String> words) {
+    return tryGetNumbersFromLongList(getLongList(words));
+  }
+
+  public static Long tryGetNumberFromWord(String word) {
+    return getFromCacheOrCompute(word);
+  }
+
+  private static Long getFromCacheOrCompute(String word) {
+    synchronized (CACHE) {
+      return CACHE.computeIfAbsent(word, NumberUtility::computeNumber);
+    }
+  }
+
+  private static List<Long> getLongList(Collection<String> words) {
+    return words.stream()
+        .filter(word -> !word.equals(conjunction))
         .map(NumberUtility::tryGetNumberFromWord)
         .collect(Collectors.toList());
-    return tryGetNumbersFromIntegerList(numbers);
   }
 
-  private static List<Long> tryGetNumbersFromIntegerList(List<Long> integers){
-    return getNumberGroupsFromIntegerList(integers).stream()
-        .filter(list -> !list.isEmpty())
+  private static Long computeNumber(String word) {
+    if (StringUtility.isNumeric(word)) {
+      return tryParse(word);
+    } else if (StringUtility.isAlphabetic(word)) {
+      return tryGetNumberFromDictionary(word);
+    }
+    return null;
+  }
+
+  private static Long tryParse(String string) {
+    try {
+      return Long.parseLong(string);
+    } catch (NumberFormatException ex) {
+      return null;
+    }
+  }
+
+  private static Long tryGetNumberFromDictionary(String word) {
+    return tryGetFromCandidates(getCandidates(WordHelper.getByWordClass(word, numberClass)));
+  }
+
+  private static ArrayList<StringCandidate> getCandidates(Collection<WordDao> words) {
+    return words.stream()
+        .map(WordDao::getDescription)
+        .map(desc -> new StringCandidate(getKeyWordMatchCount(desc) + getContainsDigitMatch(desc), desc))
+        .filter(candidate -> candidate.getMatchCount() >= minimumMatchCount)
+        .collect(Collectors.toCollection(ArrayList::new));
+  }
+
+  private static Long tryGetFromCandidates(Collection<StringCandidate> candidates) {
+    return candidates.stream()
+        .map(StringCandidate::getContent)
+        .map(StringUtility::getOnlyNumeric)
+        .map(NumberUtility::tryParse)
+        .filter(Objects::nonNull)
+        .findFirst().orElse(null);
+  }
+
+  private static List<Long> tryGetNumbersFromLongList(List<Long> list) {
+    return getNumberGroupsFromLongList(list).stream()
+        .filter(group -> !group.isEmpty())
         .map(NumberUtility::getNumberFromGroup)
         .collect(Collectors.toList());
   }
 
-  private static Long getNumberFromGroup(List<Long> group){
+  private static Long getNumberFromGroup(List<Long> group) {
     return getSubGroupsFromGroup(group).stream()
-        .filter(list -> !list.isEmpty())
+        .filter(subGroup -> !subGroup.isEmpty())
         .map(NumberUtility::getNumberByMultiplyingElements)
         .mapToLong(Long::longValue)
         .sum();
   }
 
-  private static Long getNumberByMultiplyingElements(List<Long> list){
-    Long result = 1L;
-
-    for(Long i : list){
-      result *= i;
-    }
-
-    return result;
+  private static Long getNumberByMultiplyingElements(List<Long> subGroup) {
+    return subGroup.stream().reduce(1L, (a, b) -> a * b);
   }
 
   // TODO rewrite to streams...
-  private static List<List<Long>> getSubGroupsFromGroup(List<Long> group){
+  private static List<List<Long>> getSubGroupsFromGroup(List<Long> group) {
     List<List<Long>> subGroups = new ArrayList<>();
     List<Long> currentGroup = new ArrayList<>();
 
-    for(Long i : group){
-      if (!currentGroup.isEmpty() && i <= currentGroup.get(currentGroup.size() - 1)) {
+    for (Long number : group) {
+      if (!currentGroup.isEmpty() && number <= currentGroup.get(currentGroup.size() - 1)) {
         subGroups.add(new ArrayList<>(currentGroup));
         currentGroup.clear();
       }
-      currentGroup.add(i);
+      currentGroup.add(number);
     }
 
     subGroups.add(currentGroup);
@@ -82,18 +125,18 @@ public class NumberUtility {
   }
 
   // TODO rewrite to streams...
-  private static List<List<Long>> getNumberGroupsFromIntegerList(List<Long> list){
+  private static List<List<Long>> getNumberGroupsFromLongList(List<Long> list) {
     List<List<Long>> groups = new ArrayList<>();
     List<Long> currentGroup = new ArrayList<>();
 
-    for(Long i : list){
-      if(i == null){
-        if(!currentGroup.isEmpty()){
+    for (Long number : list) {
+      if (number == null) {
+        if (!currentGroup.isEmpty()) {
           groups.add(new ArrayList<>(currentGroup));
           currentGroup.clear();
         }
-      }else{
-        currentGroup.add(i);
+      } else {
+        currentGroup.add(number);
       }
     }
 
@@ -102,59 +145,11 @@ public class NumberUtility {
     return groups;
   }
 
-  private static Long getFromCacheOrCompute(String word){
-    synchronized (CACHE){
-      return CACHE.computeIfAbsent(word, NumberUtility::computeNumber);
-    }
-  }
-
-  private static Long computeNumber(String word){
-    if(StringUtility.isNumeric(word)){
-      return tryParse(word);
-    }else if (StringUtility.isAlphabetic(word)){
-      return tryGetNumberFromDictionary(word);
-    }
-    return null;
-  }
-
-  public static Long tryGetNumberFromWord(String word){
-    return getFromCacheOrCompute(word);
-  }
-
-  private static Long tryGetNumberFromDictionary(String word){
-    return tryGetFromCandidates(getCandidates(WordHelper.getByWordClass(word, numberClass)));
-  }
-
-  private static Long tryGetFromCandidates(Collection<StringCandidate> candidates){
-    return candidates.stream()
-        .filter(candidate -> candidate.getMatchCount() >= minimumMatchCount)
-        .map(StringCandidate::getContent)
-        .map(StringUtility::getOnlyNumeric)
-        .map(NumberUtility::tryParse)
-        .filter(Objects::nonNull)
-        .findFirst().orElse(null);
-  }
-
-  private static ArrayList<StringCandidate> getCandidates(Collection<WordDao> words){
-    return words.stream()
-        .map(WordDao::getDescription)
-        .map(desc -> new StringCandidate(getKeyWordMatchCount(desc) + getContainsDigitMatch(desc), desc))
-        .collect(Collectors.toCollection(ArrayList::new));
-  }
-
-  private static int getContainsDigitMatch(String string){
+  private static int getContainsDigitMatch(String string) {
     return StringUtility.containsDigit(string) ? 1 : 0;
   }
 
-  private static long getKeyWordMatchCount(String string){
-    return Arrays.stream(keyWords).filter(word ->StringUtility.containsIgnoreCase(word, string)).count();
-  }
-
-  private static Long tryParse(String string){
-    try {
-      return Long.parseLong(string);
-    }catch (NumberFormatException ex){
-      return null;
-    }
+  private static long getKeyWordMatchCount(String string) {
+    return Arrays.stream(keyWords).filter(word -> StringUtility.containsIgnoreCase(word, string)).count();
   }
 }
