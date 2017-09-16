c459b54bd9ce19613cbc3490b6507c5854dcdded
Dariusz Lelek <dariusz.lelek@gmail.com>
2017-09-16 17:22:29 +0100

    Added TimeUtility.

---
+++ Utility/src/main/java/utilities/TimeUtility.java
@@ -0,0 +1,15 @@
+/*
+ * Created by Dariusz Lelek on 9/16/17 4:34 PM
+ * Copyright (c) 2017. All rights reserved.
+ */
+
+package utilities;
+
+public class TimeUtility {
+
+  public static boolean isTimeUnit(String text){
+
+    return false;
+  }
+
+}
