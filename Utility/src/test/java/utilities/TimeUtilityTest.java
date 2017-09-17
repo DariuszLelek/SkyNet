f52625650260fec6a6cae6b77dfe5ed654473fa0
Dariusz Lelek <dariusz.lelek@gmail.com>
2017-09-17 16:44:02 +0100

    Added tests for TimeUtility.

--- Utility/src/test/java/utilities/TimeUtilityTest.java
+++ Utility/src/test/java/utilities/TimeUtilityTest.java
@@ -5,6 +5,7 @@
 
 package utilities;
 
+import constant.Time;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
@@ -13,12 +14,42 @@ import static org.junit.Assert.*;
 public class TimeUtilityTest {
   private final static Logger logger = Logger.getLogger(TimeUtilityTest.class);
 
+  private final String[] validTimeSingular = {"Month", "Year", "Day", "later", "monthly", "january"};
+  private final String[] validTimePlural = {"months", "years", "days", "Seconds", "Hours"};
+  private final String[] invalidTime = {"Web", "people", "Bad", "Going"};
+
   @Test
   public void getTime() throws Exception {
+    logger.info("getTime - start");
+
+    for(Time fromEnum : Time.values()){
+      assertEquals(fromEnum.getDuration(), TimeUtility.getTime(fromEnum.name()).getDuration());
+    }
+
+    assertEquals(0, TimeUtility.getTime(invalidTime[0]).getDuration());
+    assertEquals(60, TimeUtility.getTime("minutes").getDuration());
+    assertEquals(3600, TimeUtility.getTime("HOURS").getDuration());
   }
 
   @Test
   public void isTime() throws Exception {
+    logger.info("isTime - start");
+
+    for(Time fromEnum : Time.values()){
+      assertTrue(TimeUtility.isTime(fromEnum.name()));
+    }
+
+    for(String singular : validTimeSingular){
+      assertTrue(TimeUtility.isTime(singular));
+    }
+
+    for(String plural : validTimePlural){
+      assertTrue(TimeUtility.isTime(plural));
+    }
+
+    for(String invalid : invalidTime){
+      assertFalse(TimeUtility.isTime(invalid));
+    }
   }
 
 }
